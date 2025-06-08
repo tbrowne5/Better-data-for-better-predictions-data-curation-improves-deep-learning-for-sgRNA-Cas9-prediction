573392021c28452c3c5a91da5f46cf4ee2f1c893
tbrowne5 <tbrowne5@uwo.ca>
2025-06-07 20:32:22 -0400

    Testing tensorflow implementation

--- processing.py
+++ processing.py
@@ -131,8 +131,8 @@ class processing:
             raise ValueError("Predictions and actual scores must have the same length.")
         
         # Calculate Spearman and Pearson correlations from numpy arrays
-        spearman_corr, _ = spearmanr(predictions, np.array(actual))
-        pearson_corr, _ = pearsonr(predictions, np.array(actual))
+        spearman_corr, _ = spearmanr(predictions, np.array(actual), axis=0)
+        pearson_corr, _ = pearsonr(predictions, np.array(actual), axis=0)
 
         print(f"Spearman correlation: {spearman_corr:.4f}")
         print(f"Pearson correlation: {pearson_corr:.4f}")
